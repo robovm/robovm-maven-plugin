5eb7032bd4d8950ff88949279ebfb83a0a9ae727
Ludovic Maître <ludovic.maitre@effervens.com>
2014-10-06 21:01:18 +0200

    Issue 9 : Also provide an archetype

---
+++ archetype/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,57 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package};
+
+import org.robovm.apple.coregraphics.CGRect;
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+import org.robovm.apple.uikit.UIApplicationDelegateAdapter;
+import org.robovm.apple.uikit.UIApplicationLaunchOptions;
+import org.robovm.apple.uikit.UIButton;
+import org.robovm.apple.uikit.UIButtonType;
+import org.robovm.apple.uikit.UIColor;
+import org.robovm.apple.uikit.UIControl;
+import org.robovm.apple.uikit.UIControlState;
+import org.robovm.apple.uikit.UIEvent;
+import org.robovm.apple.uikit.UIScreen;
+import org.robovm.apple.uikit.UIWindow;
+
+/**
+ * Hello world!
+ *
+ */
+public class App extends UIApplicationDelegateAdapter {
+
+    private UIWindow window = null;
+    private int clickCount = 0;
+
+    @Override
+    public boolean didFinishLaunching(UIApplication application,
+            UIApplicationLaunchOptions launchOptions) {
+  
+        final UIButton button = UIButton.create(UIButtonType.RoundedRect);
+        button.setFrame(new CGRect(115.0f, 121.0f, 91.0f, 37.0f));
+        button.setTitle("Click me!", UIControlState.Normal);
+
+        button.addOnTouchUpInsideListener(new UIControl.OnTouchUpInsideListener() {
+            @Override
+            public void onTouchUpInside(UIControl control, UIEvent event) {
+                button.setTitle("Click ${symbol_pound}" + (++clickCount), UIControlState.Normal);
+            }
+        });
+
+        window = new UIWindow(UIScreen.getMainScreen().getBounds());
+        window.setBackgroundColor(UIColor.lightGray());
+        window.addSubview(button);
+        window.makeKeyAndVisible();
+        
+        return true;
+    }
+
+    public static void main(String[] args) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(args, null, App.class);
+        pool.close();
+    }
+}
